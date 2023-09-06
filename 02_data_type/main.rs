c15e671c1eeffdf73cd6e17f5b0dcf6148c1d729
sxy15 <sxy_19921115@163.com>
2023-09-06 22:34:48 +0800

    feat: data type

---
+++ 02_data_type/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+  // let is used to declare a variable
+  let x = 5;
+  println!("The value of x is: {}", x);
+
+  let b = true;
+
+  let s = "Hello, world!";
+
+  println!("The value of b is: {}", b);
+
+  println!("The value of s is: {}", s);
+}
