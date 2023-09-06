d9f4906329d6194d6b8aa54be63a0646e69d244a
sxy15 <sxy_19921115@163.com>
2023-09-06 22:34:55 +0800

    feat: int

---
+++ 03_int/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+  let price = 100;
+  let price2: i32 = -100;
+  let price4: u32 = 100;
+  let price8: isize = 100;
+  let price9: usize = 100;
+
+  println!("The value of price is: {}", price);
+  println!("The value of price2 is: {}", price2);
+  println!("The value of price4 is: {}", price4);
+  println!("The value of price8 is: {}", price8);
+  println!("The value of price9 is: {}", price9);
+}
