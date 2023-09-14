f237a3002739138f20c32678997793b073e750da
sxy15 <sxy_19921115@163.com>
2023-09-14 21:26:13 +0800

    feat: move

---
+++ 15_ownership_move/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+  let name = "Rust";
+  let name2 = name;
+
+  println!("The value of name is: {}, The value of name2 is: {}", name, name2);
+  
+   // vec! 宏用于创建一个 Vec<T> 类型的对象
+  // let v1 = vec![1, 2, 3];
+  // let v2 = v1;
+  // println!("v2 is: {:?}", v1);
+}
