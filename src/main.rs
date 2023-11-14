892e4a288eb75c098a0845704af107ceb1622bdc
sxy15 <131671708+sxy15@users.noreply.github.com>
2023-11-14 11:04:15 +0000

    feat: rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 fn main() {
-    let mut s = String::from("hello");
-
-    s.push_str(", world!"); // push_str() 在字符串后追加字面值
-
-
-    println!("{}", s); // 将打印 `hello, world!`
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
 }
---
+++ src/main17.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+}
+
+fn route(ip_kind: IpAddrKind) {
+    println!("{:?}", ip_kind);
+}
