a8082d6931b6447df2ae2d242c450414d33c5172
sxy15 <sxy_19921115@163.com>
2023-11-18 23:35:50 +0800

    feat: String

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,35 @@
-// fn main() {
-//   // let v: Vec<i32> = Vec::new();
-//   // let v = vec![1, 2, 3];
-
-//   let mut v = Vec::new();
-
-//   v.push(5);
-
-//   println!("The value of v is: {:?}", v);
-
-
-// }
-
-
-// fn main() {
-//   let v = vec![1, 2, 3, 4, 5];
-
-//   let third: &i32 = &v[2];
-//   println!("The third element is {third}");
-
-//   let third: Option<&i32> = v.get(2);
-//   match third {
-//       Some(third) => println!("The third element is {third}"),
-//       None => println!("There is no third element."),
-//   }
-// }
-
-// fn main() {
-//   let mut v = vec![1, 2, 3, 4, 5];
-
-//   let first = &v[0];
-
-//   v.push(6);
-
-//   println!("The first element is: {first}"); // panic
-// }
-
-// fn main() {
-//   let v = vec![100, 32, 57];
-//   for i in &v {
-//       println!("{i}");
-//   }
-// }
-
-
-// fn main() {
-//   let mut v = vec![100, 32, 57];
-//   for i in &mut v {
-//       *i += 50;
-//   }
-// }
-
 fn main() {
-  enum SpreadsheetCell {
-      Int(i32),
-      Float(f64),
-      Text(String),
-  }
+  let mut s = String::new();
 
-  let row = vec![
-      SpreadsheetCell::Int(3),
-      SpreadsheetCell::Text(String::from("blue")),
-      SpreadsheetCell::Float(10.12),
-  ];
+  let data = "initial contents";
+  let s = data.to_string();
+
+  let s = "initial contents".to_string();
+
+
+  let s = String::from("initial contents");
+
+  // push_str push
+  let mut s = String::from("foo");
+  s.push_str("bar");
+
+  let mut s1 = String::from("foo");
+  let s2 = "bar";
+  s1.push_str(s2);
+  println!("s2 is {}", s2);
+
+  let mut s = String::from("lo");
+  s.push('l');
+
+  // + or format!
+  let s1 = String::from("Hello, ");
+  let s2 = String::from("world!");
+
+  let s3 = s1 + &s2; // 注意 s1 被移动了，不能继续使用
+  println!("s2 is {}", s2);
+
+  let s1 = String::from("tic");
+  let s2 = String::from("tac");
+  let s3 = String::from("toe");
+  let s = format!("{}-{}-{}", s1, s2, s3);
 }
-
