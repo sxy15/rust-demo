544def4ad174d254ea958755bc2f33580a3be57e
sxy15 <131671708+sxy15@users.noreply.github.com>
2023-11-20 11:29:52 +0000

    feat: T

---
+++ src/main23.rs
@@ -0,0 +1,47 @@
+// use std::fs::File;
+
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
+
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => {
+//             panic!("Problem opening the file: {:?}", error)
+//         },
+//     };
+// }
+
+// use std::fs::File;
+// use std::io::ErrorKind;
+
+// fn main() {
+//     let greeting_file_result = File::open("hello.txt");
+
+//     let greeting_file = match greeting_file_result {
+//         Ok(file) => file,
+//         Err(error) => match error.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(fc) => fc,
+//                 Err(e) => panic!("Problem creating the file: {:?}", e),
+//             },
+//             other_error => {
+//                 panic!("Problem opening the file: {:?}", other_error);
+//             }
+//         },
+//     };
+// }
+
+
+#![allow(unused)]
+fn main() {
+use std::fs::File;
+use std::io::{self, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+  }
+}
+
--- src/main24.rs
+++ src/main24.rs
@@ -1,4 +1,4 @@
-fn largest<T>(list: &[T]) -> &T {
+fn largest(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for item in list {
@@ -10,15 +10,10 @@ fn largest<T>(list: &[T]) -> &T {
     largest
 }
 
-fn main() {
-    
+fn main( ){
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
+
     println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
 }
