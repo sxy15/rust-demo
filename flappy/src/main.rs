6c3eefd0d69f8d309d829631cadd870d03b74647
sxy15 <sxy_19921115@163.com>
2023-11-25 23:40:57 +0800

    feat: flappy

---
+++ flappy/src/main.rs
@@ -0,0 +1,25 @@
+use bracket_lib::prelude::*;
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+    main_loop(context, State {})
+}
