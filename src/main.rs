b7e485d4629de69d8fa3cac7cdf84e5b39a8b116
sxy15 <sxy_19921115@163.com>
2024-01-31 02:25:49 +0000

    l

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,63 @@
-// fn apply(value: i32, f: fn(i32) -> i32) -> i32 {
-//     f(value)
-// }
+// 斐波那契数列
 
-// fn square(value: i32) -> i32 {
-//     value * value
-// }
 
-// fn cube(value: i32) -> i32 {
-//     value * value * value
-// }
-
-// fn main() {
-//     println!("apply square: {}", apply(2, square));
-//     println!("apply cube: {}", apply(2, cube));
-// } 
-
-// fn pi() -> f64 {
-//     3.1415926
-// }
-
-// fn not_pi() {
-//     3.1415926;
-// }
-
-// fn main() {
-//     let is_pi = pi();
-//     let is_unit = not_pi();
-//     let is_unit2 = {
-//         pi();
-//     };
-
-//     println!("is_pi: {:?}, is_unit: {:?}, is_unit2: {:?}", is_pi, is_unit, is_unit2); // is_pi: 3.1415926, is_unit: (), is_unit2: ()
-// }
-
-#[derive(Debug)]
-enum Gender {
-    Unspecified = 0,
-    Female = 1,
-    Male = 2,
+fn swap(a: &mut i32, b: &mut i32) {
+    let t = *a + *b;
+    *a = *b;
+    *b = t;
 }
 
-#[derive(Debug, Copy, Clone)]
-struct UserId(u64);
+fn fib_loop(n: u8) {
+    let mut a = 1;
+    let mut b = 1;
+    let mut i = 2u8;
 
-#[derive(Debug, Copy, Clone)]
-struct TopicId(u64);
+    loop {
+        swap(&mut a, &mut b);
+        i += 1;
 
-#[derive(Debug)]
-struct User {
-    id: UserId,
-    name: String,
-    gender: Gender,
+        println!("next val is {}", b);
+
+        if i >= n {
+            break;
+        }
+    }
 }
 
-#[derive(Debug)]
-struct Topic {
-    id: TopicId,
-    name: String,
-    owner: UserId,
+fn fib_while(n: u8) {
+    let mut a = 1;
+    let mut b = 1;
+    let mut i = 2u8;
+
+    while i < n {
+        swap(&mut a, &mut b);
+        i += 1;
+
+        println!("next val is {}", b);
+    }
 }
 
-#[derive(Debug)]
-enum Event {
-    Join((UserId, TopicId)),
-    Leave((UserId, TopicId)),
-    Message((UserId, TopicId, String)),
+fn fib_for(n: u8) {
+    let mut a = 1;
+    let mut b = 1;
+
+    for _i in 2..n {
+        swap(&mut a, &mut b);
+
+        println!("next val is {}", b);
+    }
 }
 
 fn main() {
-    let alice = User {id: UserId(1), name: "Alice".into(), gender: Gender::Female};
-    let bob = User {id: UserId(2), name: "Bob".into(), gender: Gender::Male};
+    let n = 10;
+    fib_loop(n);
+    fib_while(n);
+    fib_for(n);
 
-    let topic = Topic {id: TopicId(1), name: "rust".into(), owner: UserId(1)};
-
-    let event1 = Event::Join((alice.id, topic.id));
-    let event2 = Event::Join((bob.id, topic.id));
-    let event3 = Event::Message((alice.id, topic.id, "Hello, world!".into()));
-
-    println!("event1: {:?}", event1);
-    println!("event2: {:?}", event2);
-    println!("event3: {:?}", event3);
-
-    // event1: Join((UserId(1), TopicId(1)))
-    // event2: Join((UserId(2), TopicId(1)))
-    // event3: Message((UserId(1), TopicId(1), "Hello, world!"))
+    // cargo run -- https://www.rust-lang.org rust.md
+    for arg in std::env::args() {
+        println!("{}", arg); 
+        // https://www.rust-lang.org
+        // rust.md
+    }
 }
