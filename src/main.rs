cd582a3475d0548cf770e791b2da5f11f6499704
sxy15 <sxy_19921115@163.com>
2024-01-31 08:39:41 +0000

    feat: 生命周期

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
-fn sum(data: &Vec<u32>) -> u32 {
-    println!("addr of value: {:p}, addr of ref: {:p}", data, &data);
-    data.iter().fold(0, |acc, x| acc + x)
+// fn max<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+//     if s1 > s2 {
+//         s1
+//     } else {
+//         s2
+//     }
+// }
+
+// fn main() {
+//     let s1 = String::from("hello");
+//     let s2 = String::from("world");
+
+//     let result = max(&s1, &s2);
+
+//     println!("The longest string is {}", result);
+// }
+
+pub fn strtok<'a, 'b>(s: &'b mut &'a str, delimiter: char) -> &'a str {
+    if let Some(i) = s.find(delimiter) {
+        let prefix = &s[..i];
+        let suffix = &s[(i + delimiter.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else {
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
 }
+fn main() {
+    let s = "hello world".to_owned();
+    let mut s1 = s.as_str();
+    let hello = strtok(&mut s1, ' ');
 
-fn main() { // 只读引用实 现了 Copy trait，也就意味着引用的赋值、传参都会产生新的浅拷贝
-    let data = vec![1, 2, 3, 4, 5];
-    let data1 = &data;
-
-    println!("addr of value: {:p}({:p}), addr of data {:p}, data1: {:p}", &data, data1, &&data, &data1);
-
-    println!("sum of data1: {}", sum(data1));
-
-    println!(
-        "addr of items: [{:p}, {:p}, {:p}, {:p}]",
-        &data[0], &data[1], &data[2], &data[3]);
+    println!("hello is: {}, s1 is: {}, s is: {}", hello, s1, s);
 }
