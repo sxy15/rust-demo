f3c839d76e5390ae826792866c522327a06658e2
sxy15 <sxy_19921115@163.com>
2024-01-31 07:08:25 +0000

    feat: borrow

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,17 @@
-// fn main() {
-//     let data = vec![1, 42, 3, 4, 5];
-
-//     let v = 42;
-
-//     if let Some(pos) = find_pos(data, v) {
-//         println!("Found {} at position {}", v, pos);
-//     }
-// }
-
-// fn find_pos(data: Vec<i32>, value: i32) -> Option<usize> {
-//     for (i, v) in data.iter().enumerate() {
-//         if *v == value {
-//             return Some(i);
-//         }
-//     }
-//     None
-// }
-
-
-fn is_copy<T: Copy>() {}
-
-fn types_impl_copy_trait() {
-    is_copy::<bool>();
-    is_copy::<char>();
-
-    // all ixx uxx usize isize fxx types
-    is_copy::<i8>();
-    is_copy::<i64>();
-    is_copy::<u64>();
-    is_copy::<usize>();
-
-    // function (actually fn pointer)
-    is_copy::<fn()>();
-
-    // raw pointer
-    is_copy::<*const String>();
-    is_copy::<*mut String>();
-
-    // immutable reference
-    is_copy::<&String>();
-    is_copy::<&Vec<u8>>();
-
-    // array tuple with values which is copy
-    is_copy::<[i32; 3]>();
-    is_copy::<(i32, i32)>();
+fn sum(data: &Vec<u32>) -> u32 {
+    println!("addr of value: {:p}, addr of ref: {:p}", data, &data);
+    data.iter().fold(0, |acc, x| acc + x)
 }
 
-fn types_not_impl_copy_trait() {
-    // unsized or dynamically sized types
-    // is_copy::<str>();
-    // is_copy::<[i32]>();
+fn main() { // 只读引用实 现了 Copy trait，也就意味着引用的赋值、传参都会产生新的浅拷贝
+    let data = vec![1, 2, 3, 4, 5];
+    let data1 = &data;
 
-    // mutable reference
-    // is_copy::<&mut String>();
+    println!("addr of value: {:p}({:p}), addr of data {:p}, data1: {:p}", &data, data1, &&data, &data1);
 
-    // array tuple with values which is not copy
-    // is_copy::<[String; 3]>();
-}
+    println!("sum of data1: {}", sum(data1));
 
-fn main() {
-    types_impl_copy_trait();
-    types_not_impl_copy_trait();
+    println!(
+        "addr of items: [{:p}, {:p}, {:p}, {:p}]",
+        &data[0], &data[1], &data[2], &data[3]);
 }
